683f4e342428da54513474d847fccbddcacc96ee
Michael Chen <michael.chen.cse@gmail.com>
2017-03-10 18:29:19 -0500

    end of day

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const nunjucks = require('nunjucks');
 var app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
-var router = express.router();
+//var router = express.router();
+var models = require('./models');
 
 // point nunjucks to the directory containing templates and turn off caching; configure returns an Environment 
 // instance, which we'll want to use to add Markdown support later.
@@ -13,5 +14,16 @@ app.set('view engine', 'html');
 // when res.render works with html files, have it use nunjucks to do so
 app.engine('html', nunjucks.render);
 
-app.use(express.static(path.join(__dirname, '/public')));
-app.use('/', router);
+app.use(express.static('public'));
+//app.use('/', router);
+
+models.User.sync({})
+.then(function () {
+    return models.Page.sync({})
+})
+.then(function () {
+    app.listen(3000, function () {
+        console.log('Server is listening on port 3000!');
+    });
+})
+.catch(console.error);
--- models/index.js
+++ models/index.js
@@ -1,2 +1,56 @@
 var Sequelize = require('sequelize');
-var db = new Sequelize('postgres://localhost:5432/wikistack');
+var db = new Sequelize('postgres://localhost:5432/wikistack', {logging: false});
+
+var Page = db.define('page', 
+{
+	title: {
+		type: Sequelize.STRING,
+		validate: {
+			notNull: true
+		}
+	},
+	urlTitle: {
+		type: Sequelize.STRING,
+		validate: {
+			notNull: true
+		}
+	},
+	content: {
+		type: Sequelize.TEXT,
+		validate: {
+			notNull: true
+		}
+	},
+	status: {
+		type: Sequelize.ENUM('open', 'closed')
+	}
+},
+{
+	instanceMethods: {
+		getUrlTitle: function() { return this.urlTitle; }
+	}
+}
+);
+
+var User = db.define('user', {
+	name: {
+		type: Sequelize.STRING,
+		validate: {
+			notNull: true
+		}
+	},
+	email: {
+		type: Sequelize.STRING,
+		validate: {
+			isEmail: true,
+			notNull: true
+		}
+	}
+});
+
+var routes = 
+
+module.exports = {
+	Page: Page,
+	User: User
+};
